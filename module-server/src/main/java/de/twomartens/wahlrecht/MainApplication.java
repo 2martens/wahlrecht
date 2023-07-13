3fe7f1e8d532a21e41b7c44a563f465b7db73f75
Jim Martens <github@2martens.de>
2023-07-13 23:38:15 +0200

    Make sure that properties are used for OpenAPI

--- module-server/src/main/java/de/twomartens/wahlrecht/MainApplication.java
+++ module-server/src/main/java/de/twomartens/wahlrecht/MainApplication.java
@@ -1,13 +1,9 @@
 package de.twomartens.wahlrecht;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -23,15 +19,5 @@ public class MainApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(MainApplication.class, args);
-  }
-
-  @Bean
-  public OpenAPI customOpenAPI(@Value("${openapi.description}") String apiDesciption,
-      @Value("${openapi.version}") String apiVersion, @Value("${openapi.title}") String apiTitle) {
-    return new OpenAPI()
-        .info(new Info()
-            .title(apiTitle)
-            .version(apiVersion)
-            .description(apiDesciption));
   }
 }
--- module-server/src/main/java/de/twomartens/wahlrecht/configuration/OpenApiConfiguration.java
+++ module-server/src/main/java/de/twomartens/wahlrecht/configuration/OpenApiConfiguration.java
@@ -1,12 +1,12 @@
 package de.twomartens.wahlrecht.configuration;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(info = @Info(title = "Wahlrecht API", version = "v1"))
 @SecurityScheme(
     name = "basicAuth",
     type = SecuritySchemeType.HTTP,
@@ -14,5 +14,13 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class OpenApiConfiguration {
-
+  @Bean
+  public OpenAPI customOpenAPI(@Value("${openapi.description}") String apiDesciption,
+      @Value("${openapi.version}") String apiVersion, @Value("${openapi.title}") String apiTitle) {
+    return new OpenAPI()
+        .info(new io.swagger.v3.oas.models.info.Info()
+            .title(apiTitle)
+            .version(apiVersion)
+            .description(apiDesciption));
+  }
 }
