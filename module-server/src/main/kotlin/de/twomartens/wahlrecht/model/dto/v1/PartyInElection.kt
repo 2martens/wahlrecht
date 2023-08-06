a25a1f927f33c20b4a7fd0444403c7c706225204
Jim Martens <github@2martens.de>
2023-08-06 17:52:53 +0200

    Fix mapping issue

--- module-server/src/main/kotlin/de/twomartens/wahlrecht/model/dto/v1/PartyInElection.kt
+++ module-server/src/main/kotlin/de/twomartens/wahlrecht/model/dto/v1/PartyInElection.kt
@@ -1,5 +1,5 @@
 package de.twomartens.wahlrecht.model.dto.v1
 
 data class PartyInElection(var electionName: String?, var abbreviation: String?, var name: String?,
-                           var nomination: Nomination?,
+                           var overallNomination: Nomination?,
                            var constituencyNominations: Map<Int, Nomination>? = mapOf())
