a60289fc490ad92749cb74f16b85f554db234909
Jim Martens <github@2martens.de>
2023-08-16 01:00:16 +0200

    Added production server to OpenAPI

--- module-server/src/main/kotlin/de/twomartens/wahlrecht/configuration/OpenApiConfiguration.kt
+++ module-server/src/main/kotlin/de/twomartens/wahlrecht/configuration/OpenApiConfiguration.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.security.OAuthFlows
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -35,5 +36,7 @@ open class OpenApiConfiguration {
                     .version(apiVersion)
                     .description(apiDescription)
             )
+                .addServersItem(Server().url("http://localhost:12000"))
+                .addServersItem(Server().url("https://api.2martens.de"))
     }
 }
