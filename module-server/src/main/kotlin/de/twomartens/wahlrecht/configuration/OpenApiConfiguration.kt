9aa199103d758a4ab19f90f7e9b8d4fe1eeb6659
Jim Martens <github@2martens.de>
2023-08-19 18:34:25 +0200

    Don't add servers item

--- module-server/src/main/kotlin/de/twomartens/wahlrecht/configuration/OpenApiConfiguration.kt
+++ module-server/src/main/kotlin/de/twomartens/wahlrecht/configuration/OpenApiConfiguration.kt
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.security.OAuthFlows
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.servers.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -36,7 +35,5 @@ open class OpenApiConfiguration {
                     .version(apiVersion)
                     .description(apiDescription)
             )
-            .addServersItem(Server().url("http://localhost:12000"))
-            .addServersItem(Server().url("https://api.2martens.de"))
     }
 }
