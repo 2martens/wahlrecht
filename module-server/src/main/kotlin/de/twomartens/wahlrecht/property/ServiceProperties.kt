93016dabfa6af54362a0d7f876db8e7a289b2670
Jim Martens <github@2martens.de>
2025-07-26 13:37:52 +0200

    fix: Align ServiceProperties with rest of properties

--- module-server/src/main/kotlin/de/twomartens/wahlrecht/property/ServiceProperties.kt
+++ module-server/src/main/kotlin/de/twomartens/wahlrecht/property/ServiceProperties.kt
@@ -2,13 +2,12 @@ package de.twomartens.wahlrecht.property
 
 import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 import java.time.ZoneId
 
-@RefreshScope
 @ConfigurationProperties(prefix = "de.twomartens.wahlrecht")
 @Schema(description = "Properties, to configure this Application")
-open class ServiceProperties {
-    lateinit var defaultTimeZone: ZoneId
-    lateinit var greeting: String
-}
+data class ServiceProperties @ConstructorBinding constructor(
+    val defaultTimeZone: ZoneId,
+    val greeting: String
+)
